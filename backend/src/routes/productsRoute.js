4c392d688097be8f51ff36ac2c8a7b990293cce6
vieira <vini.clau0@gmail.com>
2023-06-30 13:37:24 -0300

    feat:req 04

--- backend/src/routes/productsRoute.js
+++ backend/src/routes/productsRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { productsController } = require('../controllers/index');
+const validateName = require('../middlewares/validateName');
 
 const router = express.Router();
 
 router.get('/', productsController.getAllProducts);
 router.get('/:id', productsController.getAllProductsById);
-router.post('/', productsController.insertProduct);
+router.post('/', validateName, productsController.insertProduct);
 
 module.exports = router;
